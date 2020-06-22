d3f3b432e11937b033944a95bda89a723669a338
Crowdbotics <team@crowdbotics.com>
2020-06-22 17:36:58 +0000

    Update models

--- backend/lenden_18325/wsgi.py
+++ backend/lenden_18325/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lenden_18325.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lenden_18325.settings")
 
 application = get_wsgi_application()
